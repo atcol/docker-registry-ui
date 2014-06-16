0c9a9e9243a411deb50f222840aebadcc3888113
Alex Collins <alextcollins@gmail.com>
2014-06-16 17:21:42 +0100

    Remove from domain; remove repositories field

--- src/groovy/docker/registry/web/support/Repository.groovy
+++ src/groovy/docker/registry/web/support/Repository.groovy
@@ -1,21 +1,12 @@
 package docker.registry.web.support
 
-import docker.registry.web.Registry
-
 class Repository {
     String name
-
-    static constraints = {
-    }
-
-    static belongsTo = [Registry]
 
     @Override
     public String toString() {
         return "Repository{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", version=" + version +
+                "name='" + name + '\'' +
                 '}';
     }
 }
