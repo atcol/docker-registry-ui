55f1b0dd9874395a6ef667ad2302ec939ab51e79
Alex Collins <alextcollins@gmail.com>
2014-06-16 17:22:42 +0100

    Use map for return val; add logging

--- grails-app/services/docker/registry/web/SearchService.groovy
+++ grails-app/services/docker/registry/web/SearchService.groovy
@@ -8,22 +8,25 @@ import grails.transaction.Transactional
 class SearchService {
 
     def search(final Search search) {
-        final searchResults = []
+        log.info("Search query is ${search.query}")
+        final searchResults = [:]
 
         Registry.all.each { Registry registry ->
             final uri = registry.url
-            log.info("URI is $uri")
+            log.info("search URI is $uri")
+            def imgList = []
             withHttp (uri: uri) {
                 def result = get(
                         path: "/${registry.apiVersion}/search",
                         query: [q: search.query.toString()])
                 log.info("Search result is $result")
                 result.results.each {
-                    searchResults.add(new Image(name: it.name))
+                    imgList.add(new Image(name: it.name))
                 }
+                searchResults.put(registry, imgList)
             }
-            log.info("Results are $searchResults")
         }
+        log.info("Results are $searchResults")
         searchResults
     }
 }
