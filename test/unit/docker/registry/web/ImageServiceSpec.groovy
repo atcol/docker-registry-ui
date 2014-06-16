53cb624150f37668a9710427de23f29a72dadcac
Alex <alextcollins@gmail.com>
2014-06-16 08:58:51 +0100

    Initial commit

---
+++ test/unit/docker/registry/web/ImageServiceSpec.groovy
@@ -0,0 +1,20 @@
+package docker.registry.web
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(ImageService)
+class ImageServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
---
+++ test/unit/docker/registry/web/RepositorySpec.groovy
@@ -0,0 +1,20 @@
+package docker.registry.web
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Repository)
+class RepositorySpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
