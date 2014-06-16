77e4b06e48e1678aa41b2996fc650ba0d43a78c3
Alex Collins <alextcollins@gmail.com>
2014-06-16 17:21:52 +0100

    Add toString

--- src/groovy/docker/registry/web/support/Search.groovy
+++ src/groovy/docker/registry/web/support/Search.groovy
@@ -4,4 +4,12 @@ package docker.registry.web.support
 class Search {
     String query
     List<Result> results
+
+    @Override
+    public String toString() {
+        return "Search{" +
+                "query='" + query + '\'' +
+                ", results=" + results +
+                '}';
+    }
 }
