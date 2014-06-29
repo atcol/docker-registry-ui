cda5c9039b7584b12850cf33baaa07e284e0bb6c
Alex Collins <alextcollins@gmail.com>
2014-06-29 17:37:24 +0100

    Ref #26 - create admin user if doesn't exist

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,14 @@
+import docker.registry.web.User
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (!User.findByUsername("admin")) {
+            def user = new User()
+            user.password = "pleasechangeme"
+            user.username = "admin"
+            user.save();
+        }
     }
     def destroy = {
     }
