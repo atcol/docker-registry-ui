83b6ec6c4b67cb7316cda6ce4881858197117101
Alex Collins <alextcollins@gmail.com>
2014-07-05 22:20:02 +0100

    Issue #31 - parse REG* env vars to create registries

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,4 +1,6 @@
+import docker.registry.web.Registry
 import docker.registry.web.User
+import groovyx.net.http.URIBuilder
 
 class BootStrap {
 
@@ -8,6 +10,24 @@ class BootStrap {
             user.password = "pleasechangeme"
             user.username = "admin"
             user.save();
+        }
+
+        log.info("Checking for registries in system env")
+        final def env = System.getenv()
+        env.findAll { key, urlStr ->
+            if (key.matches("REG(\\d)")) {
+                log.info("Found registry $urlStr. Creating...")
+                def m = urlStr =~ /.*(v\d).*/
+
+                if (m.matches()) {
+                    def reg = new Registry()
+                    reg.url = urlStr.replaceAll("/(v\\d)/", "") // remove API version
+                    reg.apiVersion = m.group(1) // extracts e.g. v1 from url
+                    reg.save()
+                } else {
+                    log.error("Couldn't parse the API version from $regUrl")
+                }
+            }
         }
     }
     def destroy = {
