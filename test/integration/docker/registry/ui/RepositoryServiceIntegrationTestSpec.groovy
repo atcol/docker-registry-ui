2ffee5ab255e74d8b204bdb16a30261624fab36f
Alex Collins <alextcollins@gmail.com>
2014-07-27 15:51:34 +0100

    Issue #52 - add tests for ping, index, search

---
+++ test/integration/docker/registry/ui/RepositoryServiceIntegrationTestSpec.groovy
@@ -0,0 +1,51 @@
+package docker.registry.ui
+
+import docker.registry.web.Registry
+import docker.registry.web.RepositoryService
+import grails.test.mixin.TestFor
+import spock.lang.*
+
+@TestFor(RepositoryService)
+class RepositoryServiceIntegrationTestSpec extends Specification {
+
+    final VALID_REGISTRY_URL = "http://localhost:5000/v1/"
+    final NO_SUCH_REGISTRY   = "http://bad.registry/v1/"
+    final VALID_REGISTRY     = Registry.fromUrl(VALID_REGISTRY_URL)
+    final BAD_REGISTRY       = Registry.fromUrl(NO_SUCH_REGISTRY)
+
+    def repositoryService
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test ping true on valid registry url"() {
+        when:
+            def pingResult = repositoryService.ping(VALID_REGISTRY)
+        then:
+            pingResult == true
+    }
+
+    void "test ping false on invalid registry url"() {
+        when:
+            def pingResult = repositoryService.ping(BAD_REGISTRY)
+        then:
+            !pingResult
+    }
+
+    void "test search returns non-empty collection"() {
+        when:
+            def search = repositoryService.search(VALID_REGISTRY, "ui")
+        then:
+            !search.isEmpty()
+    }
+
+    void "test index returns non-empty collection"() {
+        when:
+            def index = repositoryService.index(VALID_REGISTRY)
+        then:
+            !index.isEmpty()
+    }
+}
