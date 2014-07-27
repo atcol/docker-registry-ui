2116d798735c9a75936f565f2b4eef88f15ac36b
Alex Collins <alextcollins@gmail.com>
2014-07-27 17:55:01 +0100

    Issue #52 test index

--- test/integration/docker/registry/ui/RepositoryServiceIntegrationTestSpec.groovy
+++ test/integration/docker/registry/ui/RepositoryServiceIntegrationTestSpec.groovy
@@ -3,15 +3,15 @@ package docker.registry.ui
 import docker.registry.web.Registry
 import docker.registry.web.RepositoryService
 import grails.test.mixin.TestFor
-import spock.lang.*
+import spock.lang.Specification
 
 @TestFor(RepositoryService)
 class RepositoryServiceIntegrationTestSpec extends Specification {
 
     final VALID_REGISTRY_URL = "http://localhost:5000/v1/"
-    final NO_SUCH_REGISTRY   = "http://bad.registry/v1/"
-    final VALID_REGISTRY     = Registry.fromUrl(VALID_REGISTRY_URL)
-    final BAD_REGISTRY       = Registry.fromUrl(NO_SUCH_REGISTRY)
+    final NO_SUCH_REGISTRY = "http://bad.registry/v1/"
+    final VALID_REGISTRY = Registry.fromUrl(VALID_REGISTRY_URL)
+    final BAD_REGISTRY = Registry.fromUrl(NO_SUCH_REGISTRY)
 
     def repositoryService
 
@@ -23,29 +23,34 @@ class RepositoryServiceIntegrationTestSpec extends Specification {
 
     void "test ping true on valid registry url"() {
         when:
-            def pingResult = repositoryService.ping(VALID_REGISTRY)
+        def pingResult = repositoryService.ping(VALID_REGISTRY)
         then:
-            pingResult == true
+        pingResult == true
     }
 
     void "test ping false on invalid registry url"() {
         when:
-            def pingResult = repositoryService.ping(BAD_REGISTRY)
+        def pingResult = repositoryService.ping(BAD_REGISTRY)
         then:
-            !pingResult
+        !pingResult
     }
 
     void "test search returns non-empty collection"() {
         when:
-            def search = repositoryService.search(VALID_REGISTRY, "ui")
+        def search = repositoryService.search(VALID_REGISTRY, "ui")
         then:
-            !search.isEmpty()
+        !search.isEmpty()
     }
 
     void "test index returns non-empty collection"() {
         when:
-            def index = repositoryService.index(VALID_REGISTRY)
+        def index = repositoryService.index(VALID_REGISTRY)
         then:
-            !index.isEmpty()
+        !index.isEmpty()
+        def repo = index.pop()
+        repo.name != null
+        repo.tags != null
+        repo.tags.size() > 0
+        repo.images.size() == 0
     }
 }
