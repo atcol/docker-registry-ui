7c4bc04363da3760ab0bbde19c44813c42f3c5cc
Alex Collins <alextcollins@gmail.com>
2014-11-02 20:37:56 +0000

    Fix tomcat7 webapp path if using app ctx

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 if [ -n "$APP_CONTEXT" ]; then
-  mv /var/lib/tomcat7/webapps/ROOT.war /var/lib/webapps/${APP_CONTEXT}.war
+  mv /var/lib/tomcat7/webapps/ROOT.war /var/lib/tomcat7/webapps/${APP_CONTEXT}.war
 fi
 sed -i '1iexport CATALINA_OPTS=" -Djava.security.egd=file:/dev/./urandom -Djava.net.preferIPv6Addresses=true "' bin/catalina.sh
 bin/catalina.sh run
