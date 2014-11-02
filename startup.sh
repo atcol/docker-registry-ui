df741b8fe158b5a79ee9f6ff9f693e72ce095481
Alex Collins <alextcollins@gmail.com>
2014-11-02 20:50:24 +0000

    Closes #91; execute catalina.sh correctly

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@ if [ -n "$APP_CONTEXT" ]; then
   mv /var/lib/tomcat7/webapps/ROOT.war /var/lib/tomcat7/webapps/${APP_CONTEXT}.war
 fi
 sed -i '1iexport CATALINA_OPTS=" -Djava.security.egd=file:/dev/./urandom -Djava.net.preferIPv6Addresses=true "' bin/catalina.sh
-bin/catalina.sh run
+./catalina.sh run
 
