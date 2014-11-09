e1991235b7f364608894604fdde5f7fa5c184db5
Alex Collins <alextcollins@gmail.com>
2014-11-09 20:40:24 +0000

    Issue #99 move reachable logic to service

---
+++ test/unit/docker/registry/ui/RegistryViewServiceSpec.groovy
@@ -0,0 +1,20 @@
+package docker.registry.ui
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(RegistryViewService)
+class RegistryViewServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
