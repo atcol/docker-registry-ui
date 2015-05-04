ee7c4f339db3cc6077812cfdec6442ec79162a12
Alex Collins <alextcollins@gmail.com>
2015-05-04 09:43:42 +0100

    Issue #141 basic smoke test

--- test/unit/docker/registry/ui/RegistryViewServiceSpec.groovy
+++ test/unit/docker/registry/ui/RegistryViewServiceSpec.groovy
@@ -1,5 +1,9 @@
 package docker.registry.ui
 
+import docker.registry.web.Registry
+import docker.registry.web.RepositoryService
+import docker.registry.web.support.RegistryReposView
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -9,12 +13,30 @@ import spock.lang.Specification
 @TestFor(RegistryViewService)
 class RegistryViewServiceSpec extends Specification {
 
+    def registryViewService
+
     def setup() {
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test viewRepositories yields valid set"() {
+        given:
+        mockDomain(Registry,
+                [host: "localhost", port: 5000, apiVersion: "v1", username: "", password: "", protocol: "http"])
+        def repoSvcMock = mockFor(RepositoryService)
+        repoSvcMock.demand.index { Registry -> [new RegistryReposView(null, null, false)]}
+        registryViewService.repositoryService = repoSvcMock
+
+        when: "I view repositories"
+        final repositories = registryViewService.listRegistriesAndTheirRepositories()
+
+        then:
+        repositories != null
+        repositories.size() == 1
+        !repositories[0].isReachable
+        repositories[0].registry == null
+        repositories[0].repositories == null
     }
 }
